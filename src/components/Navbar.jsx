e32726ab2457213467385cbe6f82c2a8b21263d1
hbohlen <bohlenhayden@gmail.com>
2022-09-27 01:57:32 -0500

    Cart, Chat, Notification functionality onclick events

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
 
 
 const Navbar = () => {
-    const { activeMenu, setActiveMenu, handleClick } = useStateContext();
+    const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick } = useStateContext();
 
     return (
         <div className="flex justify-between p-2 md:mx-6 relative">
@@ -38,9 +38,61 @@ const Navbar = () => {
                     color="blue"
                     icon={<FiShoppingCart />}
 
-                >
+                />
 
-                </NavButton>
+                <NavButton
+                    title="Chat"
+                    dotColor="#03C9D7"
+                    customFunc={() => handleClick('chat')}
+                    color="blue"
+                    icon={<BsChatLeft />}
+
+                />
+
+                <NavButton
+                    title="Notification"
+                    dotColor="#03C9D7"
+                    customFunc={() => handleClick('notification')}
+                    color="blue"
+                    icon={<RiNotification3Line />}
+
+                />
+
+
+                <TooltipComponent
+                    content="Profile"
+                    position="BottomCenter"
+                >
+                    <div className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+                        onClick={() => handleClick('userProfile')}>
+
+                        <img
+                            className="rounded-full w-8 h-8"
+                            src={avatar}
+                        />
+
+                        <p>
+                            <span className="text-gray-400 text-14">Hi, </span> {' '}
+                            <span className="text-gray-400 font-bold ml-1 text-14">Michael</span>
+                        </p>
+
+                        <MdKeyboardArrowDown
+                            className="text-gray-400 text-14"
+                        />
+
+
+
+                    </div>
+
+                </TooltipComponent>
+
+                {isClicked.cart && <Cart />}
+                {isClicked.chat && <Chat />}
+                {isClicked.notification && <Notification />}
+                {isClicked.userProfile && <UserProfile />}
+
+
+
             </div>
         </div>
     )
