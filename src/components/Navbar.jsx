bc1b5d5a811bfac5c2441fad791381652da13718
hbohlen <bohlenhayden@gmail.com>
2022-09-27 02:18:02 -0500

    Navbar/Sidebar base setup complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,9 +13,9 @@ import { useStateContext } from '../contexts/ContextProvider';
 const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
     <TooltipComponent content={title} position="BottomCenter">
         <button type="button" onClick={customFunc} style={{ color }} className="relative text-xl rounded-full p-3 hover:bg-light-gray">
-            <span style={{ background: dotColor }} className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2">
-                {icon}
-            </span>
+            <span style={{ background: dotColor }} className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2" />
+            {icon}
+
         </button>
     </TooltipComponent>
 
@@ -24,7 +24,31 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
 
 
 const Navbar = () => {
-    const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick } = useStateContext();
+    const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize } = useStateContext();
+
+
+    useEffect(() => {
+        const handleResize = () => setScreenSize(window.innerWidth);
+
+        window.addEventListener('resize', handleResize);
+
+        handleResize();
+
+
+        return () => window.removeEventListener('resize', handleResize);
+
+    }, []);
+
+
+    useEffect(() => {
+        if (screenSize <= 900) {
+            setActiveMenu(false);
+        }
+        else {
+            setActiveMenu(true);
+        }
+
+    }, [screenSize]); // triggers every time screensize changes
 
     return (
         <div className="flex justify-between p-2 md:mx-6 relative">
