fb1ae24e2e0210720862c9f1c6ddfab686ac6e54
hbohlen <bohlenhayden@gmail.com>
2022-10-04 09:51:34 -0500

    Implemented Employees page with searching toolbar

---
+++ src/components/SplineComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Spline from '@splinetool/react-spline';
+
+const SplineComponent = () => {
+    return (
+        <div>
+            <Spline scene="https://prod.spline.design/6Wq1Q7YGyM-iab9i/scene.splinecode" />
+        </div>
+    )
+}
+
+export default SplineComponent
